72a3103527ced23a7a48500635fab2e55f22e53a
Andrew Pitt <apitt@redhat.com>
2022-06-01 13:37:47 -0400

    Initial commit.

---
+++ loadtest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERS=$1
+ITERATIONS=$2
+ROUTE=$3
+
+
+for (( i=0; i<$USERS; i++ ))
+do
+    curl $ROUTE -c /tmp/cookies$i
+done
+
+for (( i=0; i<$ITERATIONS; i++ ))
+do
+    
+    for (( j=0; j<$USERS; j++ ))
+    do
+	RAWOUT=$(curl $ROUTE -v -s -b /tmp/cookies$j 2>&1)
+	RIP=${RAWOUT%') port'*}
+	RIP=${RIP##*'com ('}
+
+	PIP=${RAWOUT##*'message":"'}
+	PIP=${PIP%'"}'*}
+	echo "User $j to pod IP $PIP and Router IP $RIP"
+    done
+done
+
